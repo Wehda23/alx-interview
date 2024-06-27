517d4fdaddfb723218d62b4685b17637e8ba46e4
Waheed Khaled <waheedkhaled95@gmail.com>
2024-06-27 18:24:05 +0300

    Test

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+pascale_triangle
+"""
+from typing import List
+
+
+def pascal_triangle(n: int) -> List[List]:
+    """
+    Pascal's triangle
+    """
+    if n <= 0:
+        return []
+    triangle = list()
+    for i in range(n):
+        row = list()
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(row)
+
+    return triangle
