81845bd936e1a3d4e59f0d50d241bbcdd4334668
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-04 11:30:07 +0300

    Done

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module contains function to unlockboxs"""
+
+def canUnlockAll(boxes) -> bool:
+    """Function to unlock all boxes in a list of boxes"""
+    n = len(boxes)
+    unlocked = set([0])  # Start with the first box unlocked
+    queue = [0]  # Start with the first box in the queue
+
+    while queue:
+        current_box = queue.pop(0)
+        for key in boxes[current_box]:
+            if key not in unlocked and key < n:
+                unlocked.add(key)
+                queue.append(key)
+
+    return len(unlocked) == n
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,1 @@
+# Interivew Question to UnlockBoxs
